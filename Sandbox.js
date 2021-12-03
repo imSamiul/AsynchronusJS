276879dddcd8a818e4e755374a3a6a65f14b6bb9
Samiul Karim <samiulkarimprodhan@gmail.com>
2021-12-04 00:24:58 +0600

    added status code

--- Sandbox.js
+++ Sandbox.js
@@ -1,7 +1,9 @@
 const request = new XMLHttpRequest();
 request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4) {
+  if (request.readyState === 4 && request.status === 200) {
     console.log(request.responseText);
+  } else if (request.status === 400) {
+    console.log("There was an error");
   }
 });
 request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
