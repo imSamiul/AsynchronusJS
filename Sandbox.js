7964349fd4accf1d2a50b5000b8470f032efd88d
Samiul Karim <samiulkarimprodhan@gmail.com>
2021-12-04 00:53:37 +0600

    added callback function

--- Sandbox.js
+++ Sandbox.js
@@ -1,11 +1,22 @@
-const request = new XMLHttpRequest();
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.status === 400) {
-    console.log("There was an error");
+const toDos = (callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("There is an error in the request", undefined);
+    }
+  });
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.send();
+};
+
+toDos((err, res) => {
+  console.log("Callback activated");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
   }
 });
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-request.send();
 console.log("Connected");
