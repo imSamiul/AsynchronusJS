4a30d8703cef3dadc4d5872e91c8771f5fde0f7c
Samiul Karim <samiulkarimprodhan@gmail.com>
2021-12-04 20:06:09 +0600

    Promise channing

--- Sandbox.js
+++ Sandbox.js
@@ -24,10 +24,19 @@ const toDos = (resource) => {
 // });
 
 //toDos with promises
-toDos("https://jsonplaceholder.typicode.com/todoss/")
+toDos("https://jsonplaceholder.typicode.com/todos/")
   .then((data) => {
-    console.log("Promise accepted: ", data);
+    console.log("Promise 1 accepted: ", data);
+    return toDos("https://catfact.ninja/fact");
   })
+  .then((data) => {
+    console.log("Promise 2 accepted: ", data);
+    return toDos("https://dog.ceo/api/breeds/image/random");
+  })
+  .then((data) => {
+    console.log("Promise 3 accepted: ", data);
+  })
+
   .catch((err) => {
     console.log("Promise rejected: ", err);
   });
